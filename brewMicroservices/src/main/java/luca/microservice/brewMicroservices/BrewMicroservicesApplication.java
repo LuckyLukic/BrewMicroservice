bbf0ce4a26612ac40c26fce0e6251fcb5a86fdea
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-10-25 13:33:44 +0200

    Added README.md

---
+++ brewMicroservices/src/main/java/luca/microservice/brewMicroservices/BrewMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package luca.microservice.brewMicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewMicroservicesApplication.class, args);
+	}
+
+}
---
+++ brewMicroservices/src/test/java/luca/microservice/brewMicroservices/BrewMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package luca.microservice.brewMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
