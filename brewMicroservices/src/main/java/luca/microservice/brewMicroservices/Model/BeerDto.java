053c46ead5b8097025557e734711c1fcb82a93fa
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-08 16:19:39 +0100

    Bean Validation (BeerDto)

--- brewMicroservices/src/main/java/luca/microservice/brewMicroservices/Model/BeerDto.java
+++ brewMicroservices/src/main/java/luca/microservice/brewMicroservices/Model/BeerDto.java
@@ -1,5 +1,9 @@
 package luca.microservice.brewMicroservices.Model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,29 +19,30 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @Null //Nobody can set this value from outside
     private UUID id;
-
-
+    @Null
     private Integer version;
 
-
+    @Null
     private OffsetDateTime createdDate;
-
-
+    @Null
     private OffsetDateTime lastModifiedDate;
 
-
+    @NotBlank
     private String beerName;
 
-
+    @NotNull
     private BeerStyleEnum beerStyle;
 
-
+    @NotNull
+    @Positive
     private String upc;
 
-
+    @NotNull
     private BigDecimal price;
 
+    @Null
     private Integer quantityOnHand;
 
 
